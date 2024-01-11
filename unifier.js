5f6ac39aa7bdf2023272998305338031178c2ff7
Juan <juandiegovelsol@gmail.com>
2024-01-11 17:44:09 -0500

    feat: avances en la modificacion de estilos de script unifier

--- unifier.js
+++ unifier.js
@@ -17,34 +17,95 @@ if (scriptFolder.exists) {
   var scriptNames = [];
   pushNames(scripts, scriptNames);
   var a = 0;
-  var dlg = new Window("dialog", "Seleccionar script a ejecutar");
+  var dlg = new Window("dialog", "RBupdate");
   dlg.alignChildren = ["left", "top"];
 
-  var scriptList = dlg.add("listbox", undefined, scriptNames);
-  scriptList.maximumSize.width = 300;
+  var titleBox = dlg.add("panel");
+  titleBox.text = "Selecciona el script a ejecutar";
+  titleBox.orientation = "column";
+  titleBox.alignChildren = ["left", "top"];
 
-  var btnGroup = dlg.add("group");
-  var okButton = btnGroup.add("button", undefined, "OK");
-  var cancelButton = btnGroup.add("button", undefined, "Cancelar");
+  var scriptCheckboxesGroup = titleBox.add("group");
+  scriptCheckboxesGroup.alignChildren = ["left", "top"];
+  scriptCheckboxesGroup.orientation = "column";
 
-  okButton.onClick = function () {
-    var selectedIndex = scriptList.selection.index;
-    dlg.close();
-    if (selectedIndex >= 0 && selectedIndex < scripts.length) {
-      var selectedScript = scripts[selectedIndex];
-      $.evalFile(selectedScript); // Ejecuta el script seleccionado
-    }
-  };
+  var scriptCheckboxes = [];
+  for (var i = 0; i < scriptNames.length; i++) {
+    var checkbox = scriptCheckboxesGroup.add(
+      "checkbox",
+      undefined,
+      scriptNames[i]
+    );
 
-  cancelButton.onClick = function () {
-    dlg.close();
-  };
+    // Personalizar el tamaño del checkbox
+    checkbox.preferredSize = [200, 20]; // Ajusta los valores según sea necesario
+    checkbox.index = i;
+    // Quitar el borde del checkbox
+    /* checkbox.graphics.newPath();
+    checkbox.graphics.rectPath(
+      0,
+      0,
+      checkbox.preferredSize[0],
+      checkbox.preferredSize[1]
+    );
+    checkbox.graphics.strokePath();
+    checkbox.graphics.foregroundColor = checkbox.graphics.newPen(
+      checkbox.graphics.PenType.SOLID_COLOR,
+      [1, 1, 1], // Cambia el color del fondo del checkbox según sea necesario
+      1
+    );
+    checkbox.graphics.fillPath(
+      checkbox.graphics.newBrush(
+        checkbox.graphics.BrushType.SOLID_COLOR,
+        [1, 1, 1] // Cambia el color del fondo del checkbox según sea necesario
+      )
+    ); */
+
+    // Agregar un manejador de eventos para ejecutar el script al cambiar el estado del checkbox
+    checkbox.onClick = function () {
+      if (this.value) {
+        var selectedScript = scripts[this.index];
+        this.value = false;
+        $.evalFile(selectedScript); // Ejecuta el script seleccionado
+      }
+    };
+    scriptCheckboxes.push(checkbox);
+  }
+
+  var footer = dlg.add("group");
+  footer.alignChildren = ["center", "top"];
+  footer.orientation = "row";
+
+  // Agregar logotipo orientado a la izquierda
+  var leftLogoGroup = footer.add("group");
+  leftLogoGroup.alignChildren = ["left", "top"];
+  var leftLogo = leftLogoGroup.add(
+    "image",
+    undefined,
+    File(currentScriptFolder + "/Logos/skw.png")
+  );
+  //leftLogo.preferredSize = [100, 13]; // Ajusta el tamaño según sea necesario
+
+  // Agregar campo de texto orientado a la derecha con texto y logotipo
+  var rightFieldGroup = footer.add("group");
+  rightFieldGroup.alignChildren = ["right", "top"];
+  var rightTextField = rightFieldGroup.add(
+    "statictext",
+    undefined,
+    "Built with ❤ by"
+  );
+  var rightLogo = rightFieldGroup.add(
+    "image",
+    undefined,
+    File(currentScriptFolder + "/Logos/rabbits.png")
+  );
+  //rightLogo.preferredSize = [20, 10]; // Ajusta el tamaño según sea necesario
 
   dlg.show();
 } else {
   // La carpeta "scripts" no existe
   $.writeln(
-    "La carpeta 'scripts' no existe en la ruta: " + scriptsFolder.fsName
+    "La carpeta 'scripts' no existe en la ruta: " + scriptFolder.fsName
   );
 }
 
@@ -60,3 +121,20 @@ function pushFile(scripts, scriptFiles) {
     scripts.push(scriptFiles[i]);
   }
 }
+
+/* function resizeImage(image, maxWidth, maxHeight) {
+  var imageSize = image.icon.size;
+  var aspectRatio = imageSize[0] / imageSize[1];
+
+  if (imageSize[0] > maxWidth) {
+    image.icon.size.width = maxWidth;
+    image.icon.size.height = maxWidth / aspectRatio;
+    var a = 0;
+  }
+
+  if (imageSize[1] > maxHeight) {
+    image.icon.size.width = maxHeight * aspectRatio;
+    image.icon.size.height = maxWidth;
+    var a = 0;
+  }
+} */
